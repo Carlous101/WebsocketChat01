c634762b6ebf9c2016e6fbb1da53e80346fd38a0
mabada101 <67350301+mabada101@users.noreply.github.com>
2020-08-18 15:29:58 -0500

    Create MiChat.java

---
+++ src/java/chat/MiChat.java
@@ -0,0 +1,42 @@
+package chat;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import objetos.DecoderMensaje;
+import objetos.EncoderMensaje;
+import objetos.Mensaje;
+/**
+ *
+ * @author CARLOS A.
+ */
+
+@ServerEndpoint(value="/chat", encoders = {EncoderMensaje.class}, decoders = {DecoderMensaje.class})
+
+public class MiChat {
+    
+    private static final List<Session> conectados=new ArrayList<>();
+    //Cuando se abre el socket
+    @OnOpen
+    public void inicio(Session sesion){
+        conectados.add(sesion);
+    }
+    
+    @OnClose
+    public void salir(Session sesion){
+        conectados.remove(sesion);
+    }
+    //Envia el mensaje a todas las personas que esten conectadas..
+    @OnMessage
+    public void mensaje(Mensaje mensaje) throws IOException, EncodeException{
+        for(Session sesion:conectados){
+            sesion.getBasicRemote().sendObject(mensaje);//Se le envia el mensaje, para que el cliente procese la inf
+        }
+    }
+}
